7bf5325c6e3c745d0bde876b3f4e6a42cccb7db0
HDJGJonas <jonas.haeberli@hispeed.ch>
2024-01-12 18:34:43 +0100

    fix

--- Firmware/MotorControl/encoder.cpp
+++ Firmware/MotorControl/encoder.cpp
@@ -153,6 +153,18 @@ void Encoder::set_linear_count(int32_t count) {
     cpu_exit_critical(prim);
 }
 
+void set_hw_zero_pos_offset(int32_t offset) {
+    // Disable interrupts to make a critical section to avoid race condition
+    uint32_t prim = cpu_enter_critical();
+
+    is_ready_ = false;
+
+    // Update states
+    config_.hw_zero_pos_offset = offset;
+
+    cpu_exit_critical(prim);
+}
+
 // Function that sets the CPR circular tracking encoder count to a desired 32-bit value.
 // Note that this will get mod'ed down to [0, cpr)
 void Encoder::set_circular_count(int32_t count, bool update_offset) {
@@ -799,7 +811,7 @@ bool Encoder::update() {
         } break;
     }
 
-    shadow_count_ = (pos_abs_turns_latched * config_.cpr) + pos_abs_latched - config_.hw_zero_pos_offset;
+    shadow_count_ = (pos_abs_turns_latched * config_.cpr) + pos_abs_latched;
     count_in_cpr_ = (pos_abs_turns_latched * config_.cpr) + pos_abs_latched - config_.hw_zero_pos_offset;
     count_in_cpr_ = mod(count_in_cpr_, config_.cpr);
 
@@ -837,7 +849,7 @@ bool Encoder::update() {
     }
 
     // Outputs from Encoder for Controller
-    pos_estimate_ = pos_estimate_counts_ / (float)config_.cpr;
+    pos_estimate_ = (pos_estimate_counts_ - config_.hw_zero_pos_offset) / (float)config_.cpr;
     vel_estimate_ = vel_estimate_counts_ / (float)config_.cpr;
     
     // TODO: we should strictly require that this value is from the previous iteration
